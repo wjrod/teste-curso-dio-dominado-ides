b4ded1dba5b57657220791b3a8dabc90e2747a66
wjrod <walterjrrod@hotmail.com>
2022-05-24 11:07:26 -0300

    first commit

---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.Gatoo;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+        Gatoo gato = new Gatoo();
+
+        System.out.println(gato);
+
+
+      /*int a = 5;
+       int b = 3;
+       System.out.println("Hello World!" + (a+b));*/
+    }
+}
